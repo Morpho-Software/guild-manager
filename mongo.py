5bf781dfce9e50adf5bc2d810be02e10655bdf24
Christopher Massie <chris.l.massie@gmail.com>
2022-03-12 02:26:40 -0600

    :tada:changing mongo code to a class structure

--- mongo.py
+++ mongo.py
@@ -1,19 +1,21 @@
-from typing import Collection
 import pymongo
 from pymongo import MongoClient
 
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-DB_NAME = 'guild_manager'
-COLLECTION_NAME = 'raids'
-
-
-
-client = MongoClient(MONGODB_HOST,MONGODB_PORT)
-
-db = client[DB_NAME]
-collection = db[COLLECTION_NAME]
-
-collection.insert_one({"name":"sarah","looks":"beautiful"})
-
-print("complete")
+class mongodb():
+    
+    def __init__(self,COLLECTION_NAME,DB_NAME='guild_manager',MONGODB_HOST='localhost',MONGODB_PORT=27017):
+        self.COLLECTION_NAME = COLLECTION_NAME
+        self.DB_NAME = DB_NAME
+        self.MONGODB_HOST = MONGODB_HOST
+        self.MONGODB_PORT = MONGODB_PORT
+        
+        self.client = MongoClient(MONGODB_HOST,MONGODB_PORT)
+        
+        self.db = self.client[self.DB_NAME]
+        self.collection = self.collection[self.COLLECTION_NAME]
+        
+    def insert_one_into_collection(self, item):
+        self.collection.insert_one(item)
+        
+        
+        
