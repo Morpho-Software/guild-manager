0c08ea53be77187553ca44de5088a1c19fe7e1d0
Christopher Massie <chris.l.massie@gmail.com>
2022-03-13 03:51:43 -0500

    :truck:moved mongo code

--- Utility/mongo.py
+++ Utility/mongo.py
@@ -1,7 +1,7 @@
 import pymongo
 from pymongo import MongoClient
 
-class mongodb():
+class Mongodb():
     
     def __init__(self,COLLECTION_NAME,DB_NAME='guild_manager',MONGODB_HOST='localhost',MONGODB_PORT=27017):
         self.COLLECTION_NAME = COLLECTION_NAME
@@ -27,12 +27,17 @@ class mongodb():
         self.collection.update(query,raid)
     
     def insert_new_raid(self, raid):
-        self.set_collection(self,'raids')
+        self.set_collection('raids')
         self.collection.insert_one(raid)
     
     def find_raid_by_raid_id(self, raid_id):
-        self.set_collection(self,'raids')
+        self.set_collection('raids')
         self.collection.find_one({"raidId":raid_id})
+        
+    def get_raid_count(self):
+        self.set_collection('raids')
+        count = list(self.collection.find({}))
+        return len(count)
         
     ################
     #   Setters    #
