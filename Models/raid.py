1ab9e86280bffc48ac62041ecaa337de1b24e715
Christopher Massie <chris.l.massie@gmail.com>
2022-03-13 03:50:04 -0500

    :sparkles:raid model exports itself into a dict now.

--- Models/raid.py
+++ Models/raid.py
@@ -1,21 +1,23 @@
-import datetime, sys
-from tkinter import E
+import datetime, sys, os
 from tokenize import String
 sys.path.append('..')
 from Utility.helper import open_raid_data, open_raids
+from Utility.mongo import Mongodb
 
 class raid():
     
     def __init__(self, split_incoming_message, inc_msg):
         
+        print(os.getcwd())
         rd = open_raid_data()
-        raids = open_raids()
+        #raids = open_raids()
         
         
         self.raid_name = self.validate_raid_name(rd,split_incoming_message[2])
-        self.id = f'{rd[self.raid_name]["name"]}#{len(raids)+1}'
+        mdb = Mongodb('raids')
+        self.raid_id = f'{rd[self.raid_name]["name"]}#{(mdb.get_raid_count())+1}'
         
-        
+        print(os.getcwd())
         
         split_year = split_incoming_message[3].split('/')
             
@@ -28,6 +30,8 @@ class raid():
             month=month,
             day=day
         )
+        
+        self.message_id = None
             
         self.note = split_incoming_message[5]
         self.reocurring = split_incoming_message[6],
@@ -67,3 +71,20 @@ class raid():
         if raid_name not in rd and not isValidRaid:
             raise Exception(f"Raid '{self.raid_name}' not found")
         return raid_name
+    
+    def set_confirm_message_id(self, message_id):
+        self.confirm_message_id = message_id
+    
+    def to_dict(self):
+        raid_dict = {
+            "raid_name":str(self.raid_name),
+            "raid_id":str(self.raid_id),
+            "raid_time":str(self.datetime),
+            "raid_note":self.note,
+            "raid_reoccuring":self.reocurring,
+            "raid_confirmed":self.confirmed,
+            "raid_scheduler":self.scheduler,
+            "raid_raiders":self.raiders,
+            "raid_disc_confirm_message":str(self.confirm_message_id)
+        }
+        return raid_dict
