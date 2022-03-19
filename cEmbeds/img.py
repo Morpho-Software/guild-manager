4ff746d7bcd5bd5a8e938a39f0d580c96c42cfae
Sarah Hellweg <75223568+SarahHellweg@users.noreply.github.com>
2022-03-18 23:16:27 -0500

    Create img.py

---
+++ cEmbeds/img.py
@@ -0,0 +1,2 @@
+https://cdn.discordapp.com/attachments/845526554963476491/954593602925891604/WoWScrnShot_090521_044754_-_Copy.jpg - soulbeams
+https://cdn.discordapp.com/attachments/845526554963476491/954476489879986246/SQ-Bot_300X-removebg-preview.png - bot
