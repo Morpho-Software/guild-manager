0a363e86df6d5b37b05559afa200bcff770ed2d2
Christopher Massie <chris.l.massie@gmail.com>
2022-03-19 02:42:49 -0500

    :art:minor tweaks

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ mongo = Mongodb('raids')
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
