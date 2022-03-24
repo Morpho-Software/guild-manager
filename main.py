cab230667d0a34856e61f211965d10441814b799
Christopher Massie <chris.l.massie@gmail.com>
2022-03-24 02:08:54 -0500

    :tada:new gif file

--- main.py
+++ main.py
@@ -49,8 +49,10 @@ async def on_message(message):
                 #This is a list of approved commands
                 commands = [
                     "schedule",
-                    "help",
-                    "lc"
+                    "help", #remove obselete
+                    "absent",
+                    "info",
+                    "edit"
                 ]
                 
                 #This is the command the user passed
@@ -68,8 +70,25 @@ async def on_message(message):
                         except Exception as e:
                             await message.channel.send(f'Tell Lord Gildu I am upset about: {e}')
                             return
-                    elif command == commands[2]: #lc (leadshipchat)
-                        await botcontroller.leadership_chat(bot,inc_message_split[2])
+                    elif command == commands[2]: #absent
+                        try:
+                            
+                            if len(inc_message_split) > 4:
+                                raise Exception("[BOOOOOOP]")
+                            
+                            
+                            await botcontroller.mark_raid_attendance(bot,mongo,inc_message_split)
+                        except Exception as e:
+                            await message.channel.send(f'[ALERT][ALERT]Self-Destruct Sequence Engaged... 3...')
+                            await message.channel.send(f'2....')
+                            await message.channel.send(f'1....')
+                            await message.channel.send('https://tenor.com/view/robot-freak-out-head-explode-mind-blown-gif-12345244')
+                    elif command == commands[3]: #info
+                        await botcontroller.send_raid_info_dm(bot,mongo,inc_message_split)
+                        
+                    elif command == commands[4]: #edit
+                        pass
+                        #await botcontroller.mark_raid_attendance(bot, mongo, inc_message_split)
     #Message coming from DM
     else:
         #handle naming characters
@@ -87,7 +106,9 @@ async def on_message(message):
             await botcontroller.update_raid_signup_message(raid,raid_msg)
             await botcontroller.update_raid_signup_message_mirrors(raid,bot)
         else:
-            print("nope")
+            dm = await sunhoof_member.create_dm()
+            message = await dm.send("https://tenor.com/view/puppet-awkward-looking-what-side-eye-gif-15476992")
+            
             
                     
 
